5df63f11374a754c95206c291da2f57f4c2735dc
Rui Ramos <rui@ometria.com>
2014-09-04 10:51:51 +0100

    CustomEvents polyfill

--- src/app.js
+++ src/app.js
@@ -7,8 +7,24 @@ document.querySelector('head').innerHTML += '<title>GREATDJ!</title><meta name="
 NodeList.prototype.forEach = Array.prototype.forEach;
 HTMLCollection.prototype.forEach = Array.prototype.forEach;
 
+// Custom event Polyfill for older browsers and IE
+(function () {
+  function CustomEvent ( event, params ) {
+    params = params || { bubbles: false, cancelable: false, detail: undefined };
+    var evt = document.createEvent( 'CustomEvent' );
+    evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+    return evt;
+   };
+
+  CustomEvent.prototype = window.Event.prototype;
+
+  window.CustomEvent = CustomEvent;
+})();
+
+// Start App
 require('./components/StateHandler');
 
+// Google Analytics
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
